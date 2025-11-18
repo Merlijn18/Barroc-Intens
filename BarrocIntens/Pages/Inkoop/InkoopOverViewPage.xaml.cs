b876a1e326c0896ff1b3c22c16f6afd0a43facc6
Mike <mikekramer0316@gmail.com>
2025-11-18 12:15:25 +0100

    Bestellingen overzicht

--- BarrocIntens/Pages/Inkoop/InkoopOverViewPage.xaml.cs
+++ BarrocIntens/Pages/Inkoop/InkoopOverViewPage.xaml.cs
@@ -3,99 +3,89 @@ using BarrocIntens.Models;
 using BarrocIntens.Pages.Inlog;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.System;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace BarrocIntens.Pages.Inkoop
 {
     public sealed partial class InkoopOverViewPage : Page
     {
+        // ObservableCollections voor binding aan de UI
+        private ObservableCollection<Product> Products { get; set; }
+        private ObservableCollection<Bestelling> Bestellingen { get; set; }
+
         public InkoopOverViewPage()
         {
             this.InitializeComponent();
-            LoadProduct();
 
-
+            LoadProducts();
+            LoadBestellingen();
         }
-        private void LoadProduct()
+
+        // Producten laden uit database
+        private void LoadProducts()
         {
             using var db = new AppDbContext();
-
-            var product = db.Products
-                .ToList();
-
-            ProductListView.ItemsSource = product;
-
+            var productList = db.Products.ToList();
+            Products = new ObservableCollection<Product>(productList);
+            ProductListView.ItemsSource = Products;
         }
 
+        // Bestellingen laden uit database
+        private void LoadBestellingen()
+        {
+            using var db = new AppDbContext();
+            var bestellingenList = db.Bestellingen
+                                     .OrderByDescending(b => b.Date_Of_Order)
+                                     .ToList();
 
+            Bestellingen = new ObservableCollection<Bestelling>(bestellingenList);
+            BestellingenListView.ItemsSource = Bestellingen;
+        }
+
+        // Logout knop
         private void Logout_Click(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(InlogOverViewPage));
         }
 
+        // Back knop
         private void Back_Click(object sender, RoutedEventArgs e)
         {
             Frame.GoBack();
         }
 
+        // Product search
         private void productSearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             var searchQuery = productSearchTextBox.Text;
 
             using var db = new AppDbContext();
+            var filteredProducts = db.Products
+                                     .Where(p => p.Productname.Contains(searchQuery) ||
+                                                 p.Stock.ToString().Contains(searchQuery))
+                                     .OrderByDescending(p => p.Productname)
+                                     .ToList();
 
-            ProductListView.ItemsSource = db.Products
-                .Where(p => p.Productname.Contains(searchQuery) || p.Stock.ToString().Contains(searchQuery))
-                .OrderByDescending(p => p.Productname)
-                .ToList();
-
-
+            Products = new ObservableCollection<Product>(filteredProducts);
+            ProductListView.ItemsSource = Products;
         }
 
+        // Toon producten knop
         private void ShowProducts_Click(object sender, RoutedEventArgs e)
         {
-            using (var context = new AppDbContext())
-            {
-                var products = context.Products.ToList();
-
-                ProductListView.ItemsSource = products;
-                ProductListView.Visibility = Visibility.Visible;
-
-                // Toon de terug-knop
-                HideProductsButton.Visibility = Visibility.Visible;
-
-                // Verberg de ShowProductsButton
-                ShowProductsButton.Visibility = Visibility.Collapsed;
-
-                ((Button)sender).Visibility = Visibility.Collapsed;
-            }
+            ProductListView.Visibility = Visibility.Visible;
+            HideProductsButton.Visibility = Visibility.Visible;
+            ShowProductsButton.Visibility = Visibility.Collapsed;
         }
-        
 
+        // Verberg producten knop
         private void HideProductsButton_Click(object sender, RoutedEventArgs e)
         {
             ProductListView.Visibility = Visibility.Collapsed;
-
             HideProductsButton.Visibility = Visibility.Collapsed;
-
             ShowProductsButton.Visibility = Visibility.Visible;
         }
     }
 }
-
